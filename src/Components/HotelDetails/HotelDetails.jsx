cbe6ca5fb5e3437ccdd9a5412d7f541a14c96d39
Md. Shoaib Akhter Shifat <sifuisu3@gmail.com>
2023-11-27 23:44:22 +0600

    rooms added

--- src/Components/HotelDetails/HotelDetails.jsx
+++ src/Components/HotelDetails/HotelDetails.jsx
@@ -1,35 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const HotelDetails = () => {
+  const [rooms, setRooms] = useState([]);
+  const [category, setCategory] = useState("");
+  useEffect(() => {
+    axios.get("http://localhost:5000/rooms").then((res) => setRooms(res.data));
+  }, [setRooms]);
+  // console.log(rooms);
+
   return (
     <div className="relative">
       <img
         src="https://images.unsplash.com/photo-1625244724120-1fd1d34d00f6?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aG90ZWxzfGVufDB8fDB8fHww"
         alt=""
-        className="absolute inset-0 min-h-screen w-screen object-cover object-center bg-fixed overflow-hidden"
+        className="absolute min-h-screen md:max-h-screen w-screen object-cover object-center overflow-hidden"
       />
-      <div className="absolute inset-0 min-h-screen min-w-screen bg-opacity-60  backdrop-blur-sm bg-gray-800"></div>
+      <div className="absolute inset-0 min-h-screen min-w-screen bg-opacity-60 backdrop-blur-sm bg-gray-800"></div>
       <div className="container mx-auto p-5 relative text-white">
-        <div className="mb-20">
-          <h2 className="text-5xl font-bold mb-2">Hotel Name</h2>
-          <span>Hotel Location</span>
-        </div>
-        <div className="flex flex-col gap-5 md:flex-row md:items-center md:justify-between">
-          <div className="flex flex-col bg-dark p-5 shadow-md rounded-lg md:w-1/2">
-            <h2 className="text-xl font-semibold mb-4">Family Room</h2>
-            <ul className="flex gap-2 list">
-              <li>3 Beds</li>
-              <li>1 Balcony</li>
-              <li>2 Washrooms</li>
-            </ul>
-            <span>Number of rooms: 5</span>
-            <span className="italic mt-3">
-              <strong className="not-italic">BDT 200</strong> per night
-            </span>
-            <button className="bg-primary mx-auto p-2 max-w-max text-white mt-5 rounded-md cursor-pointer active:bg-secondary">
-              Book Now!
-            </button>
+        <div className="mb-20 flex flex-col md:flex-row md:items-center md:justify-between">
+          <div>
+            <h2 className="text-5xl font-bold mb-2">Hotel Name</h2>
+            <span>Hotel Location</span>
           </div>
+          <div className="flex flex-col gap-2">
+            <span className="mt-5">Browse Rooms By Category</span>
+            <select
+              className="text-gray-800 p-2"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="">All Category</option>
+              <option>Family Room</option>
+              <option>King Size Room</option>
+              <option>Couple Room</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-[repeat(1,_1fr)] md:grid-cols-[repeat(2,_1fr)] lg:grid-cols-[repeat(3,_1fr)] gap-5">
+          {category
+            ? rooms
+                .filter(
+                  (room) =>
+                    room.category.toLowerCase().trim() ===
+                    category.toLowerCase().trim()
+                )
+                .map((room) => (
+                  <div
+                    className="flex flex-col bg-dark p-5 shadow-md rounded-lg"
+                    key={room.id}
+                  >
+                    <h2 className="text-xl font-semibold mb-4">
+                      {room.roomTitle}
+                    </h2>
+                    <ul className="flex gap-2 list">
+                      <li>{room.roomDetails.bed} Beds</li>
+                      <li>{room.roomDetails.balcony} Balcony</li>
+                      <li>{room.roomDetails.washroom} Washrooms</li>
+                    </ul>
+
+                    <span className="italic mt-3">
+                      <strong className="not-italic">BDT {room.price}</strong>{" "}
+                      per night
+                    </span>
+                    <button
+                      type="button"
+                      className="bg-primary mx-auto p-2 max-w-max text-white mt-5 rounded-md cursor-pointer active:bg-secondary disabled:opacity-50 disabled:cursor-not-allowed disabled:active:bg-primary"
+                      disabled={room.booked === true}
+                    >
+                      Book Now!
+                    </button>
+                  </div>
+                ))
+            : rooms.map((room) => (
+                <div
+                  className="flex flex-col bg-dark p-5 shadow-md rounded-lg"
+                  key={room.id}
+                >
+                  <h2 className="text-xl font-semibold mb-4">
+                    {room.roomTitle}
+                  </h2>
+                  <ul className="flex gap-2 list">
+                    <li>{room.roomDetails.bed} Beds</li>
+                    <li>{room.roomDetails.balcony} Balcony</li>
+                    <li>{room.roomDetails.washroom} Washrooms</li>
+                  </ul>
+                  <span className="italic mt-3">
+                    <strong className="not-italic">BDT {room.price}</strong> per
+                    night
+                  </span>
+                  <button
+                    type="button"
+                    className="bg-primary mx-auto p-2 max-w-max text-white mt-5 rounded-md cursor-pointer active:bg-secondary disabled:opacity-50 disabled:cursor-not-allowed disabled:active:bg-primary"
+                    disabled={room.booked === true}
+                  >
+                    Book Now!
+                  </button>
+                </div>
+              ))}
         </div>
       </div>
     </div>
